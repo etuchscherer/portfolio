bb9a3130e7ea10f2a44224aefa74775713d7db35
Eric Tuchscherer <erictuchscherer@Erics-iMac.westell.com>
2019-04-29 22:23:57 -0700

    Have a basic page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,44 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
+    <v-container fluid>
+      <v-layout column align-center justify-center fill-height>
+        <v-flex shrink>
+          <h1 class="display-4 font-weight-regular darkest--text">Eric Tuchscherer</h1>
+        </v-flex>
+        <v-flex shrink>
+          <ul class="list-inline">
+            <li class="px-2">
+              <a target="_new" class="social-link display-1 grey--text darken-4" href="https://twitter.com/e_tuchscherer">twitter</a>
+            </li>
+            <li class="px-2">
+              <a target="_new" class="social-link display-1 grey--text darken-4" href="https://www.facebook.com/eric.tuchscherer">facebook</a>
+            </li>
+            <li class="px-2">
+              <a target="_new" class="social-link display-1 grey--text darken-4" href="https://www.linkedin.com/in/tuchscherer/">linkedin</a>
+            </li>
+            <li class="px-2 social-link">
+              <a target="_new" class="social-link display-1 grey--text darken-4" href="https://github.com/etuchscherer">github</a>
+            </li>
+          </ul>
+        </v-flex>
+      </v-layout>
+    </v-container>
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data () {
-    return {
-      //
-    }
+<style lang="scss" scoped>
+  .list-inline {
+    display: inline-flex;
+    list-style-type: none
   }
-}
-</script>
+
+  .darkest--text {
+    color: #333;
+  }
+
+  .social-link:hover {
+    // use !important otherwise material design overrides
+    color: #333 !important;
+  }
+</style>
