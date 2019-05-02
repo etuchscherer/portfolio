b33fbd76263c48d3553d4e204396fbcd4dfc58bd
Eric Tuchscherer <eric@tuchscherer.me>
2019-05-01 23:08:17 -0700

    Adding drawer

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,15 @@
   <v-app>
     <v-container fluid>
       <VueGithubCorners :repoUrl="repoUrl" :catColor="catColor" :bgColor="bgColor"></VueGithubCorners>
+
+      <seattle-drawer :open="shouldOpenDrawer" v-on:isClosed="drawerClosed"/>
+
       <v-layout column align-center justify-center fill-height>
         <v-flex shrink>
           <h1 class="display-4 font-weight-regular darkest--text">Eric Tuchscherer</h1>
         </v-flex>
         <v-flex shrink>
-          <h4 class="display-2 grey--text darken-4 my-2">Software Engineer from Seattle, WA</h4>
+          <h4 class="display-2 grey--text darken-4 my-2">Software Engineer from <span @click="doOpenDrawer()" class="hoverable">Seattle, WA</span></h4>
         </v-flex>
         <v-flex shrink>
           <ul class="list-inline my-4">
@@ -34,16 +37,32 @@
 <script>
 import Vue from 'vue';
 import { VueGithubCorners } from 'vue2-github-corners';
+import SeattleDrawer from './components/seattle-drawer.vue';
 
 // https://github.com/WeiChiaChang/vue-github-corners
 Vue.component('VueGithubCorners', VueGithubCorners);
+Vue.component('seattle-drawer', SeattleDrawer);
 
 export default {
   data() {
     return {
+      shouldOpenDrawer: false,
+
+      // vue-github-corner configuration
       repoUrl: 'https://github.com/etuchscherer/portfolio',
       bgColor: '#333',
       catColor: '#FFF'
+    }
+  },
+  components: {
+    SeattleDrawer
+  },
+  methods: {
+    drawerClosed() {
+      this.shouldOpenDrawer = false;
+    },
+    doOpenDrawer() {
+      this.shouldOpenDrawer = true;
     }
   }
 }
@@ -63,5 +82,9 @@ export default {
   .social-link:hover {
     // use !important otherwise material design overrides
     color: #333 !important;
+  }
+
+  span.hoverable:hover {
+    color: red;
   }
 </style>
---
+++ src/components/seattle-drawer.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-navigation-drawer v-model="isOpen" absolute temporary dark right>
+    <v-toolbar flat>
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-title class="title">
+            About Seattle
+          </v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-toolbar>
+
+    <v-divider />
+
+    <v-list>
+      <v-list-tile>
+        <a class="subheading white--text" href="#">History</a>
+      </v-list-tile>
+      <v-list-tile>
+        <a class="subheading white--text" href="#">Weather</a>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ['open'],
+  data() {
+    return {
+      isOpen: false
+    }
+  },
+  watch: {
+    open(val) {
+      // why tf do I need 2 watchers to
+      // toggle this shit. Lame.
+      if (val) this.isOpen = val;
+    },
+    isOpen(val) {
+      this.isOpen = val;
+      this.$emit('isClosed', val)
+    }
+  }
+}
+</script>
