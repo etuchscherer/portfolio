613ca8d7c867781154a4feea8deeac46b6232f8c
Eric Tuchscherer <eric.tuchscherer@peopleconnect.us>
2019-11-04 15:43:31 -0800

    Adding Analytics

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+  id: 'UA-151507138-2'
+});
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
